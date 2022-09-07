6eaf98878864514e0c72ff798effdaa14c9ec1bc
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2022-09-07 10:30:16 -0500

    Prácticas con archivos planos

---
+++ APlanos/app/src/main/java/com/example/aplanos/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.aplanos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEscribe;
+    Button btnOprimir;
+    TextView tvResultad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        conexion();
+    }
+
+    private void conexion() {
+    }
+}
