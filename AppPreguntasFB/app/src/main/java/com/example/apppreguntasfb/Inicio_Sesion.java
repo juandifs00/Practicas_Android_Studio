3593096b77236062cf9577d338fc1294994ac6ac
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2022-10-21 15:39:59 -0500

    Primer commit app preguntas con firebase

---
+++ AppPreguntasFB/app/src/main/java/com/example/apppreguntasfb/Inicio_Sesion.java
@@ -0,0 +1,14 @@
+package com.example.apppreguntasfb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio_Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_sesion);
+    }
+}
