70732e44af79d79df892f8b44ec7377f2c24e195
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2022-11-21 23:25:08 -0600

    Mostrar las estaciones y su información

--- SIATA/app/src/main/java/com/example/siata/MapsActivity.java
+++ SIATA/app/src/main/java/com/example/siata/MapsActivity.java
@@ -5,16 +5,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.siata.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
@@ -31,14 +34,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        AñadirEstaciones();
-
-        //double Distancia = DistanciaNodos();
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     public void AñadirEstaciones() {
@@ -61,6 +61,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         PosEstaciones.add(new Estaciones("82","Copacabana", 6.3453598, -75.5047531, 12));
         PosEstaciones.add(new Estaciones("101","Girardota", 6.3732505, -75.4483109, 8));
         PosEstaciones.add(new Estaciones("81","Barbosa", 6.4369602, -75.3303986, 7));
+    }
+
+    final double MI_LATITUD = 6.16697164311577;
+    final double MI_LONGITUD = 75.57830929756166;
+
+    public static double Distancias(LatLng StartP, LatLng EndP) {
+
+        final double RADIO_TIERRA = 6371.01;
+
+        double Lat1 = StartP.latitude;
+        double Lat2 = EndP.latitude;
+        double Long1 = StartP.longitude;
+        double Long2 = EndP.longitude;
+        double dLat = Math.toRadians(Lat2 - Lat1);
+        double dLong = Math.toRadians(Long2 - Long1);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(Lat1))
+                * Math.cos(Math.toRadians(Lat2)) * Math.sin(dLong / 2)
+                * Math.sin(dLong / 2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = RADIO_TIERRA * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.parseInt(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.parseInt(newFormat.format(meter));
+        Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                + " Meter   " + meterInDec);
+
+        return RADIO_TIERRA * c;
     }
 
     public static double DistanciaNodos(double latitudNodo1, double longitudNodo1,
@@ -88,10 +120,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         googleMap.setOnMarkerClickListener(this);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        AñadirEstaciones();
+
+        for (int i = 0; i<PosEstaciones.size(); i++) {
+
+            String Codigo = PosEstaciones.get(i).getCodigo();
+            String Municipio = PosEstaciones.get(i).getMunicipio();
+            double Latitud = PosEstaciones.get(i).getLatitud();
+            double Longitud = PosEstaciones.get(i).getLongitud();
+            int Concentracion = PosEstaciones.get(i).getConcentracion();
+
+            LatLng Marcadores = new LatLng(Latitud, Longitud);
+
+            mMap.addMarker(new MarkerOptions().position(Marcadores).icon(BitmapDescriptorFactory.defaultMarker
+                    (BitmapDescriptorFactory.HUE_VIOLET)).title("Codigo:" + Codigo + " " + "Municipio:" + Municipio
+                    + " " + "Concentracion:" + Concentracion));
+        }
     }
 
     @Override
     public boolean onMarkerClick(@NonNull Marker marker) {
         return false;
     }
+
+    double [][] coordenadas = new double[19][19];
+
 }
